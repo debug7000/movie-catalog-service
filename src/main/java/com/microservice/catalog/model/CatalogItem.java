6f608f414fbbefbce62e43466f6f81d5072ab31e
deepak kumar <debug7000@gmail.com>
2020-03-10 12:16:28 +0530

    movie-catalog-service-with-eureka

---
+++ src/main/java/com/microservice/catalog/model/CatalogItem.java
@@ -0,0 +1,38 @@
+package com.microservice.catalog.model;
+
+public class CatalogItem {
+	
+	private int rating;
+	private String name;
+	private String desc;
+	
+	
+	public CatalogItem( String name, String desc,int rating) {
+		super();
+		this.rating = rating;
+		this.name = name;
+		this.desc = desc;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+
+	
+
+}
